0ac19fe10a6340f77368979abee3e9f4418289c2
Zac Gross <brickman1444@gmail.com>
2019-01-09 19:33:35 -0600

    Move Overworld room to the top of the list to make it easy to find.

--- InitialRooms.cs
+++ InitialRooms.cs
@@ -19,6 +19,23 @@ namespace GridExplorerBot
 
         public static Dictionary<InitialRooms.ID,Room> initialRooms = new Dictionary<InitialRooms.ID, Room>()
         {
+            [InitialRooms.ID.Overworld] = new Room( "The World",
+                    new string[] {
+                    "🌍🌍🌍🌍🌍🌍🌍🌍🌍",
+                    "🌍⬜⬜⬜⬜⬜⬜⬜🌍",
+                    "🌍⬜⬜⬜⬜⬜⬜⬜🌍",
+                    "🌍⬜⬜⬜⬜⬜⬜⬜🌍",
+                    "🌍⬜⬜⬜⬜⬜⬜⬜🌍",
+                    "🌍⬜⬜⬜⬜⬜⬜⬜🌍",
+                    "🌍⬜⬜⬜⬜⬜⬜⬜🌍",
+                    "🌍⬜⬜⬜⬜⬜⬜⬜🌍",
+                    "🌍🌍🌍🌍🌍🌍🌍🌍🌍", },
+                   new GridObjectSetup[] {
+                       new GridObjectSetup(Emoji.Player.Default, new Point(5,4)),
+                       new DoorSetup(Emoji.Buildings.Castle, new Point(4,4), InitialRooms.ID.VampireCastleCourtyard, new Point(4,7), "Vampire Castle"),
+                       new DoorSetup(Emoji.Plants.ChristmasTree, new Point(4,2), InitialRooms.ID.Overworld, new Point(4,2), "Magic Forest"),
+                       new DoorSetup(Emoji.Environment.SatelliteAntenna, new Point(4,6), InitialRooms.ID.Overworld, new Point(4,6), "Research Facility"),
+                       } ),
             [ID.Circus] = new Room( "The Circus",
                     new string[] {
                    "⬛⬛⬛⬛⬛⬛⬛⬛⬛",
@@ -55,23 +72,6 @@ namespace GridExplorerBot
                        new GridObjectSetup(Emoji.Plants.Rose, new Point(1,6)),
                        new GridObjectSetup(Emoji.InventoryItems.Candle, new Point(3,7)),
                        new GridObjectSetup(Emoji.InventoryItems.Candle, new Point(5,7)),
-                       } ),
-            [InitialRooms.ID.Overworld] = new Room( "The World",
-                    new string[] {
-                    "🌍🌍🌍🌍🌍🌍🌍🌍🌍",
-                    "🌍⬜⬜⬜⬜⬜⬜⬜🌍",
-                    "🌍⬜⬜⬜⬜⬜⬜⬜🌍",
-                    "🌍⬜⬜⬜⬜⬜⬜⬜🌍",
-                    "🌍⬜⬜⬜⬜⬜⬜⬜🌍",
-                    "🌍⬜⬜⬜⬜⬜⬜⬜🌍",
-                    "🌍⬜⬜⬜⬜⬜⬜⬜🌍",
-                    "🌍⬜⬜⬜⬜⬜⬜⬜🌍",
-                    "🌍🌍🌍🌍🌍🌍🌍🌍🌍", },
-                   new GridObjectSetup[] {
-                       new GridObjectSetup(Emoji.Player.Default, new Point(5,4)),
-                       new DoorSetup(Emoji.Buildings.Castle, new Point(4,4), InitialRooms.ID.VampireCastleCourtyard, new Point(4,7), "Vampire Castle"),
-                       new DoorSetup(Emoji.Plants.ChristmasTree, new Point(4,2), InitialRooms.ID.Overworld, new Point(4,2), "Magic Forest"),
-                       new DoorSetup(Emoji.Environment.SatelliteAntenna, new Point(4,6), InitialRooms.ID.Overworld, new Point(4,6), "Research Facility"),
                        } ),
         };
     }
